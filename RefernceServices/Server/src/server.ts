6e618c2b776d5f2113643c8e479d959bbecf0f87
chaniji <chaniji19@gmail.com>
2026-06-25 11:13:04 +0530

    Express App init with helment,compression,proxy and Basics Routes Init /,/health,/test

--- RefernceServices/Server/src/server.ts
+++ RefernceServices/Server/src/server.ts
@@ -1,13 +1,7 @@
 import { env } from "./config/env.js";
+import app from "./app.js";
 import { log } from "./utils/logger.js";
 
-log.info("Server config", {
-  port: env.port,
-  nodeEnv: env.nodeEnv,
-  corsOrigin: env.corsOrigin,
-  appUrl: env.appUrl,
-  emailFromAddress: env.emailFromAddress,
-  emailFromName: env.emailFromName,
-  sendGridApiKey: env.sendGridApiKey ? "***" : "NOT SET",
-  jwtSecret: env.jwtSecret ? "***" : "NOT SET",
+const server = app.listen(env.port, () => {
+  log.info(`Server running on port ${env.port} in ${env.nodeEnv} mode`);
 });
